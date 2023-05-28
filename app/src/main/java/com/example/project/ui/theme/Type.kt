ca42ed69d92b8f7f771071cce631267706845739
Malaika Basharat <criticalangel786@gmail.com>
2023-05-28 19:15:44 +0500

    updated

--- app/src/main/java/com/example/project/ui/theme/Type.kt
+++ app/src/main/java/com/example/project/ui/theme/Type.kt
@@ -2,11 +2,16 @@ package com.example.project.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.project.R
 
 // Set of Material typography styles to start with
+val ProductSans = FontFamily(
+    Font(R.font.product_sans_regular)
+)
 val Typography = Typography(
     bodyLarge = TextStyle(
         fontFamily = FontFamily.Default,
@@ -15,6 +20,7 @@ val Typography = Typography(
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp
     )
+
     /* Other default text styles to override
     titleLarge = TextStyle(
         fontFamily = FontFamily.Default,
